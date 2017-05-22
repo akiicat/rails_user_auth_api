082dcbf03e7136032521b9822311ffc3fbac474f
akiicat <aaaa1379@gmail.com>
2017-05-23 01:01:06 +0800

    fix devise api jwt bug

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AuthenticationController < ApiController
-  skip_before_action :authenticate_token!
+  skip_before_action :authenticate_user!
 
   def create
-    user = User.find_by(email: params[:user][:email])
-    if user.valid_password? params[:user][:password]
+    user = User.find_by(email: params[:auth][:email])
+    if user.valid_password? params[:auth][:password]
       render json: { token: JsonWebToken.encode(sub: user.id) }
     else
       render json: { errors: ["Invalid email or password"] }
